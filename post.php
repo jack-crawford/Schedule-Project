2ef387819f711489243a436099c0fe58e33df9d3
JackWork <jackcrawford@tenstreets-Mac-mini.local>
2015-11-23 17:43:11 -0600

    moved a function around

--- post.php
+++ post.php
@@ -9,19 +9,88 @@ if (!$db_server) die("Unable to connect at line 7" . mysql_error());
 mysql_select_db($db_database)
   or die('unable to select database: ' . mysql_error());
 mysql_select_db($db_database, $db_server) or die("unable to connect at line 7 " . mysql_error());
-$newdayoff = $_POST['dayoff'];
-$offdaytable = mysql_query('SELECT * FROM offdays');
-$adddaysoff = "INSERT INTO offdays(numdate) VALUES ('$newdayoff') ON DUPLICATE KEY UPDATE numdate=numdate";
-$dayoffresult = mysql_query($adddaysoff);
-if(!$dayoffresult) die ('database access failed: . ' . mysql_error());
-if(!$offdaytable) die ('database access failed: . ' . mysql_error());
 
+$newdayoff = $_POST['dayoff'];
+
+//when a day is added it's posted to offdays
+$adddayoff = "INSERT INTO offdays(numdate) VALUES ('$newdayoff') EXCEPT SELECT * FROM offdays";
+$dayoffresult = mysql_query($adddayoff);
+if(!$dayoffresult) die ('database access failed: . ' . mysql_error());
+
+//When days are added to offdays they are deleted from days
+$deletedaysofffromdays = "DELETE FROM days WHERE numdate = '$newdayoff'";
+$deletedaysofffromdaysresult = mysql_query($deletedaysofffromdays);
+if(!$deletedaysofffromdaysresult) die ('database access failed: . ' . mysql_error());
 
 $offdays = array('11.25.15','11.26.15','11.27.15');
 $offdayssql = array();
 
 while($row = mysql_fetch_array($offdaytable)){
     $offdayssql[] = $row['numdate'];
+
 }
+
+//updatedays() writes an $x's worth of values to days
+function updatedays($x){
+  $offdaytable = mysql_query('SELECT * FROM offdays');
+  if(!$offdaytable) die ('database access failed: . ' . mysql_error());
+  while ($x === 365):
+  //if it's a weekend, skip
+    if (date('D' , strtotime("+ $x day")) === "Sun" or date('D' , strtotime("+ $x day")) === "Sat"){
+      $x = $x + 1;
+    } elseif (in_array(date('m.d.y', strtotime("+ $x day")), $offdayssql)) {
+      $x = $x + 1;
+    } else {
+      //if it's a weekday, echo it.
+      $extrapolateddate = date('l F d ', strtotime("+ $x day"));
+      echo $extrapolateddate;
+      $formattedextrapolateddate = date('m.d.y', strtotime("+ $x day"));
+
+        if ($cyc == 1) {
+          echo "- A Day ";
+          $letter = "A";
+          global $letter;
+          $cyc = $cyc + 1;
+        }
+        elseif ($cyc == 2) {
+          echo "- B Day ";
+          $letter = "B";
+          global $letter;
+          $cyc = $cyc + 1;
+        }
+        elseif ($cyc == 3) {
+          echo "- C Day ";
+          $letter = "C";
+          global $letter;
+          $cyc = $cyc + 1;
+        }
+        elseif ($cyc == 4) {
+          echo "- D Day, Short Classes ";
+          $letter = "D";
+          global $letter;
+          $cyc = $cyc + 1;
+        }
+        elseif ($cyc == 5) {
+          echo "- E Day ";
+          $letter = "E";
+          global $letter;
+          $cyc = $cyc + 1;
+        }
+        elseif ($cyc == 6) {
+          echo "- F Day ";
+          $letter = "F";
+          global $letter;
+          $cyc = 1;
+        }
+      $dayquery = "INSERT INTO days VALUES ('$formattedextrapolateddate','$letter') EXCEPT SELECT * FROM days";
+      $result = mysql_query($dayquery);
+      if(!$result) die ('database access failed: . ' . mysql_error());
+        echo $b;
+        $x = $x + 1;
+      }
+    endwhile;
+}
+
+
 
 ?>
--- schedule.php
+++ schedule.php
@@ -19,63 +19,8 @@ echo "Add days off: <form action='post.php' method='post'><input type='text' nam
 $today = date('m.d.y');
 //starting letter
 $letter = "D";
-//extrapolate to 30 days
-while ($x === 365):
-  //if it's a weekend, skip
-  if (date('D' , strtotime("+ $x day")) === "Sun" or date('D' , strtotime("+ $x day")) === "Sat"){
-    $x = $x + 1;
-  } elseif (in_array(date('m.d.y', strtotime("+ $x day")), $offdayssql)) {
-    $x = $x + 1;
-  } else {
-    //if it's a weekday, echo it.
-    $extrapolateddate = date('l F d ', strtotime("+ $x day"));
-    echo $extrapolateddate;
-    $formattedextrapolateddate = date('m.d.y', strtotime("+ $x day"));
 
-      if ($cyc == 1) {
-        echo "- A Day ";
-        $letter = "A";
-        global $letter;
-        $cyc = $cyc + 1;
-      }
-      elseif ($cyc == 2) {
-        echo "- B Day ";
-        $letter = "B";
-        global $letter;
-        $cyc = $cyc + 1;
-      }
-      elseif ($cyc == 3) {
-        echo "- C Day ";
-        $letter = "C";
-        global $letter;
-        $cyc = $cyc + 1;
-      }
-      elseif ($cyc == 4) {
-        echo "- D Day, Short Classes ";
-        $letter = "D";
-        global $letter;
-        $cyc = $cyc + 1;
-      }
-      elseif ($cyc == 5) {
-        echo "- E Day ";
-        $letter = "E";
-        global $letter;
-        $cyc = $cyc + 1;
-      }
-      elseif ($cyc == 6) {
-        echo "- F Day ";
-        $letter = "F";
-        global $letter;
-        $cyc = 1;
-      }
-      $dayquery = "INSERT INTO days VALUES ('$formattedextrapolateddate','$letter') EXCEPT SELECT * FROM days";
-      $result = mysql_query($dayquery);
-      if(!$result) die ('database access failed: . ' . mysql_error());
-echo $b;
-$x = $x + 1;
 
-}
-endwhile;
 $todayquery = "SELECT cycleday FROM days WHERE numdate = '$today'";
 
 $todayresult = mysql_query($todayquery);
