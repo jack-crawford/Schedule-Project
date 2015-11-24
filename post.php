5422df7b09ba9203bde606ce50de5fa35e989f1c
JackWork <jackcrawford@tenstreets-Mac-mini.local>
2015-11-23 18:09:54 -0600

    minor changes

--- post.php
+++ post.php
@@ -31,65 +31,80 @@ while($row = mysql_fetch_array($offdaytable)){
 }
 
 //updatedays() writes an $x's worth of values to days
-function updatedays($x){
-  $offdaytable = mysql_query('SELECT * FROM offdays');
-  if(!$offdaytable) die ('database access failed: . ' . mysql_error());
-  while ($x === 365):
-  //if it's a weekend, skip
-    if (date('D' , strtotime("+ $x day")) === "Sun" or date('D' , strtotime("+ $x day")) === "Sat"){
-      $x = $x + 1;
-    } elseif (in_array(date('m.d.y', strtotime("+ $x day")), $offdayssql)) {
-      $x = $x + 1;
-    } else {
-      //if it's a weekday, echo it.
-      $extrapolateddate = date('l F d ', strtotime("+ $x day"));
-      echo $extrapolateddate;
-      $formattedextrapolateddate = date('m.d.y', strtotime("+ $x day"));
 
-        if ($cyc == 1) {
-          echo "- A Day ";
-          $letter = "A";
-          global $letter;
-          $cyc = $cyc + 1;
+$offdaytable = mysql_query('SELECT * FROM offdays');
+if(!$offdaytable) die ('database access failed: . ' . mysql_error());
+    //if it's a weekend, skip
+  if (date('D' , strtotime("+ $x day")) === "Sun" or date('D' , strtotime("+ $x day")) === "Sat"){
+      $x = $x + 1;
+  } elseif (in_array(date('m.d.y', strtotime("+ $x day")), $offdayssql)) {
+      $x = $x + 1;
+  } else {
+      //if it's a weekday, echo it.
+    $extrapolateddate = date('l F d ', strtotime("+ $x day"));
+    echo $extrapolateddate;
+    $formattedextrapolateddate = date('m.d.y', strtotime("+ $x day"));
+
+      if ($cyc == 1) {
+        echo "- A Day ";
+        $letter = "A";
+        global $letter;
+        $cyc = $cyc + 1;
+        $dayquery = "UPDATE days(cycleday) VALUES ('$letter')";
+        $result = mysql_query($dayquery);
+        if(!$result) die ('database access failed: . ' . mysql_error());
+      }
+      elseif ($cyc == 2) {
+        echo "- B Day ";
+        $letter = "B";
+        global $letter;
+        $cyc = $cyc + 1;
+        $dayquery = "UPDATE days(cycleday) VALUES ('$letter')";
+        $result = mysql_query($dayquery);
+        if(!$result) die ('database access failed: . ' . mysql_error());
+      }
+      elseif ($cyc == 3) {
+        echo "- C Day ";
+        $letter = "C";
+        global $letter;
+        $cyc = $cyc + 1;
+        $dayquery = "UPDATE days(cycleday) VALUES ('$letter')";
+        $result = mysql_query($dayquery);
+        if(!$result) die ('database access failed: . ' . mysql_error());
+      }
+      elseif ($cyc == 4) {
+        echo "- D Day, Short Classes ";
+        $letter = "D";
+        global $letter;
+        $cyc = $cyc + 1;
+        $dayquery = "UPDATE days(cycleday) VALUES ('$letter')";
+        $result = mysql_query($dayquery);
+        if(!$result) die ('database access failed: . ' . mysql_error());
+      }
+      elseif ($cyc == 5) {
+        echo "- E Day ";
+        $letter = "E";
+        global $letter;
+        $cyc = $cyc + 1;
+        $dayquery = "UPDATE days(cycleday) VALUES ('$letter')";
+        $result = mysql_query($dayquery);
+        if(!$result) die ('database access failed: . ' . mysql_error());
         }
-        elseif ($cyc == 2) {
-          echo "- B Day ";
-          $letter = "B";
-          global $letter;
-          $cyc = $cyc + 1;
-        }
-        elseif ($cyc == 3) {
-          echo "- C Day ";
-          $letter = "C";
-          global $letter;
-          $cyc = $cyc + 1;
-        }
-        elseif ($cyc == 4) {
-          echo "- D Day, Short Classes ";
-          $letter = "D";
-          global $letter;
-          $cyc = $cyc + 1;
-        }
-        elseif ($cyc == 5) {
-          echo "- E Day ";
-          $letter = "E";
-          global $letter;
-          $cyc = $cyc + 1;
-        }
-        elseif ($cyc == 6) {
-          echo "- F Day ";
-          $letter = "F";
-          global $letter;
-          $cyc = 1;
-        }
-      $dayquery = "INSERT INTO days VALUES ('$formattedextrapolateddate','$letter') EXCEPT SELECT * FROM days";
-      $result = mysql_query($dayquery);
-      if(!$result) die ('database access failed: . ' . mysql_error());
+      elseif ($cyc == 6) {
+        echo "- F Day ";
+        $letter = "F";
+        global $letter;
+        $cyc = 1;
+        $dayquery = "UPDATE days(cycleday) VALUES ('$letter')";
+        $result = mysql_query($dayquery);
+        if(!$result) die ('database access failed: . ' . mysql_error());
+      }
+
         echo $b;
         $x = $x + 1;
       }
-    endwhile;
-}
+endwhile;
+
 
 
 
