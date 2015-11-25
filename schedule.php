6e87bbc3d7254b44250d5f0e0059b95eeb50e538
JackWork <jackcrawford@tenstreets-Mac-mini.local>
2015-11-25 12:35:22 -0600

    next: fix results of today

--- schedule.php
+++ schedule.php
@@ -9,18 +9,18 @@ if ($db_server->connect_errno) {
 $letter = "D";
 $x = 1;
 $cyc = 1;
-$today = '12.12.15';
-//date('m.d.y');
+$today = date('m.d.y');
 
 //theoretically add days to offdays array
 echo "Add days off: <form action='post.php' method='post'><input type='text' name='dayoff' /><input type='submit' /> </form> ";
 
-$todayquery = "SELECT cycleday FROM days WHERE numdate = '$today'";
+$todayquery = "SELECT * FROM days WHERE numdate = '$today'";
 $todayresult = mysqli_real_query($db_server, $todayquery);
 
 if ($todayresult->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-print $todayresult;
-var_dump($todayresult);
+$varresult = $result->fetch_assoc($todayresult);
+echo $b;
+return $varresult;
 ?>
